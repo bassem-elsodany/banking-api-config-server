701cd7d731255cd67ce61b6688f5089de57f5ebe
Bassem Elsodany <bassem.elsodany@gmail.com>
2021-04-12 15:41:46 +0100

    init

---
+++ src/test/java/com/epam/boot/api/bankingapiconfigserver/BankingApiConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epam.boot.api.bankingapiconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingApiConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
